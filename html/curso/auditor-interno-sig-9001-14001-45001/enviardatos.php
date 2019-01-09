7d83de20fe90209d6e5f5f2c7884765da8a6adff
Juan Quintanilla <quintanilla.peru@gmail.com>
2019-01-09 01:04:10 -0500

    mas html

--- html/curso/auditor-interno-sig-9001-14001-45001/enviardatos.php
+++ html/curso/auditor-interno-sig-9001-14001-45001/enviardatos.php
@@ -1,55 +1,10 @@
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-ini_set('allow_url_fopen', 1);
-ini_set('allow_url_include', 1);
-error_reporting(E_ALL);
-echo '1';
 if (count($_POST) == 0) {
 	header('location: https://www.facebook.com/AENORPeru/');
 	exit;
 }
-echo '2';
+
 date_default_timezone_set('America/Lima');
-
-
-/*
-
-require_once 'recaptcha-master/src/autoload.php';
-$recaptcha = new \ReCaptcha\ReCaptcha('6Lcmx4QUAAAAAIQ493nUteCVHLtzBFZFfZkra5xy');
-$resp = $recaptcha->setExpectedHostname('certificacionperu.com')
-                  ->verify($_POST['g-recaptcha-response'], $_SERVER['REMOTE_ADDR']);
-echo '<pre>';
-print_r($resp);
-
-if ($resp->isSuccess()) {
-    echo 'Verified!';
-} else {
-    $errors = $resp->getErrorCodes();
-}
-print_r($errors);echo '</pre>';exit;
-
-// Build POST request:
-$recaptcha_url = 'https://www.google.com/recaptcha/api/siteverify';
-$recaptcha_secret = '6Lcmx4QUAAAAAIQ493nUteCVHLtzBFZFfZkra5xy';
-$recaptcha_response = $_POST['g-recaptcha-response'];
-
-print_r($_POST); //exit;
-
-// Make and decode POST request:
-$recaptcha = file_get_contents($recaptcha_url . '?secret=' . $recaptcha_secret . '&response=' . $recaptcha_response);
-$recaptcha = json_decode($recaptcha);
-
-print_r($recaptcha);exit;
-
-// Take action based on the score returned:
-if ($recaptcha->score >= 0.5) {
-    // Verified - send email
-} else {
-    // Not verified - show form error
-}
-*/
 
 $fuente = strip_tags($_POST['fuente']);
 $titulo = 'Formulario ' . strip_tags($_POST['titulo']);
@@ -62,11 +17,11 @@ $html = "<strong>$titulo</strong><br><br>Una solicitud de información recibida:
 $respuesta = $_SERVER['HTTP_REFERER'] . '?rpta=ok';
 
 $to = 'quintanilla.peru@gmail.com'; //formacion.peru@aenor.com
-$headers = "From: Juan Carlos Quintanilla <quintanilla.peru@gmail.com> \r\n";
+$headers = "From: $nombre <$correo> \r\n";
 $headers .= "Reply-To: $nombre <$correo> \r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
-echo '3';
+
 if (mail($to, $titulo, $html, $headers)) 
 header('location: ' . $respuesta);
 
